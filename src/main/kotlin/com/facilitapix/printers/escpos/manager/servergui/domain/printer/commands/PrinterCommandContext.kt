f828e4944877d6f5d3b474e2ff8975f8bcf49ad5
moacirjun <moacirjunior.eletro@gmail.com>
2024-09-21 19:18:41 -0300

    Removing unused class.

--- src/main/kotlin/com/facilitapix/printers/escpos/manager/servergui/domain/printer/commands/PrinterCommandContext.kt
+++
@@ -1,9 +0,0 @@
-package com.facilitapix.printers.escpos.manager.servergui.domain.printer.commands
-
-import com.facilitapix.printers.escpos.manager.servergui.domain.server.OrderReceipt
-import com.github.anastaciocintra.escpos.EscPos
-
-class PrinterCommandContext(
-    val printer: EscPos,
-    val orderReceipt: OrderReceipt,
-)
